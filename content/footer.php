89bcbae733e6cbacb0a3ee95f23297074ee97c45
Chris Kunz <Dian@Christians-MacBook-Pro.local>
2016-03-07 11:26:44 +0100

    PHP IF-Abfrage im Footer.
    - if Abfrage im nav_footer hinzugefügt.
    - if == 'true' add class="active_footer".
    - css: .active_footer: color #b3b3b3.

--- content/footer.php
+++ content/footer.php
@@ -8,10 +8,10 @@
 
     <ul class="nav_footer">
       <li><a href="#">&copy; Lotus Yoga</a></li>
-      <li><a href="index.php?page=Impressum">Impressum</a></li>
-      <li><a href="index.php?page=FAQs">FAQs</a></li>
-      <li><a href="index.php?page=AGB">AGB</a></li>
-      <li><a href="index.php?page=Datenschutz">Datenschutz</a></li>
+      <li><a href="index.php?page=Impressum"<?php if( $page == "Impressum" ) echo 'class="active_footer"'; ?>>Impressum</a></li>
+      <li><a href="index.php?page=FAQs"<?php if( $page == "FAQs" ) echo 'class="active_footer"'; ?>>FAQs</a></li>
+      <li><a href="index.php?page=AGB"<?php if( $page == "AGB" ) echo 'class="active_footer"'; ?>>AGB</a></li>
+      <li><a href="index.php?page=Datenschutz"<?php if( $page == "Datenschutz" ) echo 'class="active_footer"'; ?>>Datenschutz</a></li>
     </ul>
   </footer>
 
