42d55afc4606b218187516b33825276058094007
Chris Kunz <Dian@Christians-MacBook-Pro.local>
2016-03-08 21:34:30 +0100

    Abschluss.php.
    Wrapper:
    - order-confirmation geadded
    - css styling
    - funktioniert nicht ganz

--- content/Abschluss.php
+++ content/Abschluss.php
@@ -1,14 +1,20 @@
-<section class="content-wrapper">
+<section class="content-wrapper order-confirmation">
 
-    <p>Vielen Dank für Ihre Bestellung! <br />Sie erhalten in kürze eine E-Mail mit Ihrer Bestell-Bestätigung.</p>
-    <p>Sollten Sie diese nicht erhalten kontaktieren Sie uns bitte unter <a href="mailto:office@lotusyoga.at?subject=Kontaktformular Website">office@lotusyoga.at</a> oder  01 555 555 55.</p>
+  <section class="bg-wrapper">
+    <h3>Vielen Dank für Ihre Bestellung! <br /></h3>
+    <p>Sie erhalten in kürze eine E-Mail mit Ihrer Bestell-Bestätigung,
+      sollten Sie diese nicht erhalten kontaktieren Sie uns bitte unter
+      <a href="mailto:office@lotusyoga.at?subject=Kontaktformular Website">office@lotusyoga.at</a>
+      oder  01 555 555 55.</p>
 
      <div class="#">
           <a href="index.php?page=Shop" class="#">Zum Shop</a>
      </div>
 
     <div class="#">
-        <a href="index.php?page=home" class="#">Zur Startseite</a>
+        <a href="index.php?page=home#about_us" class="#">Zur Startseite</a>
     </div>
+
+  </section>
 
 </section>
