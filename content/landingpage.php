f4c275d2cede57ff1651f9d4c1b00d356fd8bb8e
cK <dian@Christians-MBP.home>
2016-07-25 23:18:46 +0200

    somethings.

--- content/landingpage.php
+++ content/landingpage.php
@@ -2,7 +2,7 @@
 
     <h4 class="landingpage_headline"><span class="light">Willkommen <br> bei </span><br>Lotus Yoga</h4>
 
-    <div class="btn_wrapper">
+    <!-- <div class="btn_wrapper">
       <div class="btn_landingpage">
         <a href="#about_us">Klick Mich</a>
       </div>
@@ -10,9 +10,6 @@
       <div class="btn_landingpage">
         <a href="index.php?page=Shop">Zum Shop</a>
       </div>
-    </div>
+    </div> -->
 
   </div>
-
-
-<!-- <?php include ('../header.php'); ?> -->
