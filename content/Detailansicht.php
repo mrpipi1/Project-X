74e500b76cf42278c425c69c5c7b259abd440a8d
Christopher Giustiniani <giustiniani.christopher@gmail.com>
2016-07-27 12:41:10 +0200

    Detailansicht from db Colors and Sizes need to be optimised

--- content/Detailansicht.php
+++ content/Detailansicht.php
@@ -1,56 +1,68 @@
 <section class="content-wrapper wrapper_checkout">
 <?php
     while( $row = mysqli_fetch_assoc($detail_product) ) {
-    print_r($row);
-        ?>
+?>
 
 
-        <section class="pictures_wrapper">
+    <section class="pictures_wrapper">
 
-            <div class="zoomed-picture">
-                <a href="#"><img id="detail_img" src="<?php echo $row['pic'] ?>" alt="<?php echo $row['_name'] ?>"></a>
-            </div>
+        <div class="zoomed-picture">
+            <a href="#"><img id="detail_img" src="<?php echo $row['pic'] ?>" alt="<?php echo $row['_name'] ?>"></a>
+        </div>
 
-            <ul class="thumbs">
-                <li class="pic_detailansicht">
-                    <div><img class="img-container" src="<?php echo $row['thumb_1'] ?>"
-                              alt="<?php echo $row['_name'] ?>"></div>
-                </li>
-                <li class="pic_detailansicht">
-                    <div><img class="img-container" src="<?php echo $row['thumb_2'] ?>"
-                              alt="<?php echo $row['_name'] ?>"></div>
-                </li>
-                <li class="pic_detailansicht">
-                    <div><img class="img-container" src="<?php echo $row['thumb_3'] ?>"
-                              alt="<?php echo $row['_name'] ?>"></div>
-                </li>
-            </ul>
+        <ul class="thumbs">
+            <li class="pic_detailansicht">
+                <div><img class="img-container" src="<?php echo $row['thumb_1'] ?>"
+                          alt="<?php echo $row['_name'] ?>"></div>
+            </li>
+            <li class="pic_detailansicht">
+                <div><img class="img-container" src="<?php echo $row['thumb_2'] ?>"
+                          alt="<?php echo $row['_name'] ?>"></div>
+            </li>
+            <li class="pic_detailansicht">
+                <div><img class="img-container" src="<?php echo $row['thumb_3'] ?>"
+                          alt="<?php echo $row['_name'] ?>"></div>
+            </li>
+        </ul>
 
+    </section>
+
+    <section class="info-wrapper_detailansicht">
+        <section class="headline-wrapper_detailansicht">
+            <h2><?php echo $row['_name'] ?></h2>
+            <h2><?php echo $row['price'] ?></h2>
         </section>
 
-        <section class="info-wrapper_detailansicht">
-            <section class="headline-wrapper_detailansicht">
-                <h2><?php echo $row['_name'] ?></h2>
-                <h2><?php echo $row['price'] ?></h2>
-            </section>
-
-            <p><?php echo $row['description'] ?></p>
-
-
+        <p><?php echo $row['description'] ?></p>
+        <?php
+        }
+            ?>
             <div class="selects">
                 <p class="selects-text">Größe:</p>
-                <div class="size sizepicker-xs">XS</div>
-                <div class="size sizepicker-s">S</div>
+                <p class="selects-text">Farbe:</p>
+                <?php
+                while( $row2 = mysqli_fetch_assoc($stock_color) ) {
+                    ?>
+                <div class="selects">
+                    <div
+                        class="size sizepicker-<?php echo $row2['size'] ?>"><?php echo $row2['size'] ?></div>
+</div>
+                <!--<div class="size sizepicker-s">S</div>
                 <div class="size sizepicker-m">M</div>
                 <div class="size sizepicker-l">L</div>
-                <div class="size sizepicker-xl">XL</div>
-            </div>
+                <div class="size sizepicker-xl">XL</div>-->
 
-            <div class="selects">
-                <p class="selects-text">Farbe:</p>
-                <div class="color colorpicker-black"></div>
-                <div class="color colorpicker-red"></div>
-                <div class="color colorpicker-white"></div>
+
+
+
+                <div class="selects">
+                <div class="color colorpicker-<?php echo $row2['color'] ?>">
+                    </div>
+                    <?php
+                }
+                ?>
+                <!--<div class="color colorpicker-red"></div>
+                <div class="color colorpicker-white"></div>-->
             </div>
 
 
@@ -67,9 +79,7 @@
             </form>
 
         </section>
-        <?php
-    }
-    ?>
+
 
 
 
