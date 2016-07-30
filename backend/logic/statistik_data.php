b417d6449743c290d8a6388ae76a9c1c31d1c147
Alexandra Benkö <ab@codeq.at>
2016-07-30 12:14:54 +0200

    statistiken fertig

--- backend/logic/statistik_data.php
+++ backend/logic/statistik_data.php
@@ -2,17 +2,12 @@
 include("../../db-connect.php");
 $tablename = $_POST['statistik_table'];
 $column = $_POST['statistik_col'];
-$value = $_POST['satistik_value'];
-$tablename2 = $_POST['statistik_table2'];
-$column2 = $_POST['statistik_col2'];
-$value2 = $_POST['satistik_value2'];
-//function get_count($table_name, $where, $equals){
+$values = $_POST['satistik_values'];
   global $link;
-  $sql = "SELECT * FROM ".$tablename." WHERE ".$column. " = '" .$value."'";
-  $res = mysqli_query($link, $sql);
-  $result[] = mysqli_num_rows($res);
-  $sql2 = "SELECT * FROM ".$tablename2." WHERE ".$column2. " = '" .$value2."'";
-  $res2 = mysqli_query($link, $sql2);
-  $result[] = mysqli_num_rows($res2);
-// }
+  for($i = 0; $i < count($values); $i++){
+    $sql = "SELECT * FROM ".$tablename." WHERE ".$column. " = '" .$values[$i]."'";
+    $res = mysqli_query($link, $sql);
+    $result[] = mysqli_num_rows($res);
+  }
+
   echo json_encode($result);
--- backend/statistic.js
+++ backend/statistic.js
@@ -1,102 +1,103 @@
 /**
  * Created by alex on 05/05/16.
  */
-
- function statistik_data_Query(tablename, column, value, tablename2, column2, value2) {
-     $.post('logic/statistik_data.php', {statistik_table: tablename, statistik_col: column, satistik_value: value, statistik_table2: tablename2, statistik_col2: column2, satistik_value2: value2}, function(response, status) {
+ function statistik_data_Query(tablename, column, values, colors) {
+   var data = [];
+     $.post('logic/statistik_data.php', {statistik_table: tablename, statistik_col: column, satistik_values: values}, function(response, status) {
          if(status == 'success') {
-             console.log(response);
-             response = jQuery.parseJSON(response);;
+            response = jQuery.parseJSON(response);
+            console.log(response);
+            for(i = 0; i < response.length; i++){
+              if(response[i] > 0){
+                data.push( {
+                    "label": values[i],
+                    "value": response[i],
+                    "color": colors[i]
+                });
+              }
+                if(i == response.length - 1){
+                  var pie = new d3pie("pieChart", {
+                      "header": {
+                          "title": {
+                              "text": column,
+                              "fontSize": 24,
+                              "font": "helvetica"
+                          },
+                          "subtitle": {
+                              "text": "",
+                              "color": "#999999",
+                              "fontSize": 10,
+                              "font": "helvetica"
+                          },
+                          "location": "pie-center",
+                          "titleSubtitlePadding": 10
+                      },
+                      "footer": {
+                          "color": "#999999",
+                          "fontSize": 10,
+                          "font": "open sans",
+                          "location": "bottom-left"
+                      },
+                      "size": {
+                          "canvasHeight": 300,
+                          "canvasWidth": 340,
+                          "pieInnerRadius": "92%",
+                          "pieOuterRadius": "72%"
+                      },
+                      "data": {
+                          "sortOrder": "label-desc",
+                          "content":
+                              data
 
-             var pie = new d3pie("pieChart", {
-                 "header": {
-                     "title": {
-                         "text": "gender",
-                         "fontSize": 24,
-                         "font": "helvetica"
-                     },
-                     "subtitle": {
-                         "text": "",
-                         "color": "#999999",
-                         "fontSize": 10,
-                         "font": "helvetica"
-                     },
-                     "location": "pie-center",
-                     "titleSubtitlePadding": 10
-                 },
-                 "footer": {
-                     "color": "#999999",
-                     "fontSize": 10,
-                     "font": "open sans",
-                     "location": "bottom-left"
-                 },
-                 "size": {
-                     "canvasHeight": 300,
-                     "canvasWidth": 340,
-                     "pieInnerRadius": "92%",
-                     "pieOuterRadius": "72%"
-                 },
-                 "data": {
-                     "sortOrder": "label-desc",
-                     "content": [
-                         {
-                             "label": value,
-                             "value": response[0],
-                             "color": "#3f9999"
-                         },
-                         {
-                             "label": value2,
-                             "value": response[1],
-                             "color": "#206999"
-                         }
-                     ]
-                 },
-                 "labels": {
-                     "outer": {
-                         "format": "label-percentage1",
-                         "pieDistance": 10
-                     },
-                     "inner": {
-                         "format": "none"
-                     },
-                     "mainLabel": {
-                         "font": "helvetica",
-                         "fontSize": 11
-                     },
-                     "percentage": {
-                         "color": "#999999",
-                         "font": "helvetica",
-                         "fontSize": 11,
-                         "decimalPlaces": 0
-                     },
-                     "value": {
-                         "color": "#cccc43",
-                         "font": "helvetica",
-                         "fontSize": 11
-                     },
-                     "lines": {
-                         "enabled": true,
-                         "style": "straight",
-                         "color": "#777777"
-                     },
-                     "truncation": {
-                         "enabled": true
-                     }
-                 },
-                 "effects": {
-                     "pullOutSegmentOnClick": {
-                         "effect": "linear",
-                         "speed": 400,
-                         "size": 8
-                     }
-                 },
-                 "misc": {
-                     "colors": {
-                         "segmentStroke": "#000000"
-                     }
-                 }
-             });
+                      },
+                      "labels": {
+                          "outer": {
+                              "format": "label-percentage1",
+                              "pieDistance": 10
+                          },
+                          "inner": {
+                              "format": "none"
+                          },
+                          "mainLabel": {
+                              "font": "helvetica",
+                              "fontSize": 11
+                          },
+                          "percentage": {
+                              "color": "#999999",
+                              "font": "helvetica",
+                              "fontSize": 11,
+                              "decimalPlaces": 0
+                          },
+                          "value": {
+                              "color": "#cccc43",
+                              "font": "helvetica",
+                              "fontSize": 11
+                          },
+                          "lines": {
+                              "enabled": true,
+                              "style": "straight",
+                              "color": "#777777"
+                          },
+                          "truncation": {
+                              "enabled": true
+                          }
+                      },
+                      "effects": {
+                          "pullOutSegmentOnClick": {
+                              "effect": "linear",
+                              "speed": 400,
+                              "size": 8
+                          }
+                      },
+                      "misc": {
+                          "colors": {
+                              "segmentStroke": "#000000"
+                          }
+                      }
+                  });
+                }
 
+             }
 
          }else{
            console.log('error');
@@ -104,102 +105,8 @@
      });
 
  }
-statistik_data_Query('users', 'gender', 'female', 'users', 'gender', 'male');
-//statistik_data_Query('users', 'pref_delivery', 'female', 'users', 'gender', 'male');
-
-
-    //####################
-
-    // var pie = new d3pie("pieChart", {
-    //     "header": {
-    //         "title": {
-    //             "text": "prev. delivery",
-    //             "fontSize": 24,
-    //             "font": "helvetica"
-    //         },
-    //         "subtitle": {
-    //             "text": "",
-    //             "color": "#999999",
-    //             "fontSize": 10,
-    //             "font": "helvetica"
-    //         },
-    //         "location": "pie-center",
-    //         "titleSubtitlePadding": 10
-    //     },
-    //     "footer": {
-    //         "color": "#999999",
-    //         "fontSize": 10,
-    //         "font": "open sans",
-    //         "location": "bottom-left"
-    //     },
-    //     "size": {
-    //         "canvasHeight": 300,
-    //         "canvasWidth": 340,
-    //         "pieInnerRadius": "92%",
-    //         "pieOuterRadius": "72%"
-    //     },
-    //     "data": {
-    //         "sortOrder": "label-desc",
-    //         "content": [
-    //             {
-    //                 "label": "Post",
-    //                 "value": 8,
-    //                 "color": "#3f9999"
-    //             },
-    //             {
-    //                 "label": "DHL",
-    //                 "value": 3,
-    //                 "color": "#709925"
-    //             },
-    //             {
-    //                 "label": "Express",
-    //                 "value": 6,
-    //                 "color": "#206999"
-    //             }
-    //         ]
-    //     },
-    //     "labels": {
-    //         "outer": {
-    //             "format": "label-percentage1",
-    //             "pieDistance": 10
-    //         },
-    //         "inner": {
-    //             "format": "none"
-    //         },
-    //         "mainLabel": {
-    //             "font": "helvetica",
-    //             "fontSize": 11
-    //         },
-    //         "percentage": {
-    //             "color": "#999999",
-    //             "font": "helvetica",
-    //             "fontSize": 11,
-    //             "decimalPlaces": 0
-    //         },
-    //         "value": {
-    //             "color": "#cccc43",
-    //             "font": "helvetica",
-    //             "fontSize": 11
-    //         },
-    //         "lines": {
-    //             "enabled": true,
-    //             "style": "straight",
-    //             "color": "#777777"
-    //         },
-    //         "truncation": {
-    //             "enabled": true
-    //         }
-    //     },
-    //     "effects": {
-    //         "pullOutSegmentOnClick": {
-    //             "effect": "linear",
-    //             "speed": 400,
-    //             "size": 8
-    //         }
-    //     },
-    //     "misc": {
-    //         "colors": {
-    //             "segmentStroke": "#000000"
-    //         }
-    //     }
-    // });
+ // here are the colors for the statistics defined
+var colors = ['#000', '#848484', '#dddddd'];
+statistik_data_Query('users', 'gender', ['female', 'male'], colors);
+statistik_data_Query('users', 'pref_delivery', ['DHL', 'Post', 'express'], colors);
+statistik_data_Query('users', 'pref_payment', ['Rechnung', 'Vorkasse', 'Kreditkarte'], colors);
